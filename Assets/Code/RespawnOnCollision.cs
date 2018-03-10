9d0cc4ab60d34d5488f5d1231a2f41fa1a2b48dd
Kris Krej <kris@krej.net>
2018-03-10 18:45:56 +0100

    Respawn when drawn

---
+++ Assets/Code/RespawnOnCollision.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnOnCollision : MonoBehaviour {
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player")
+            SpawnPoint.ResetPlayerPosition();
+    }
+}
---
+++ Assets/WaterMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMovement : MonoBehaviour {
+    float y0, r1, r2;
+    public float maxY = 1f;
+
+    void Start() {
+        y0 = transform.position.y;
+        r1 = Random.Range(0f, 1f);
+        r2 = Random.Range(0f, maxY);
+    }
+	
+	void Update () {
+	    var y = y0 + Mathf.Sin(Time.timeSinceLevelLoad * r1) * r2;
+	    transform.position = new Vector3(transform.position.x, y, transform.position.z);
+	}
+}
