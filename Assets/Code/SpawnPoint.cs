b04b4f2acfdde7e70bbc79c9109f4e0e90f769e0
Kris Krej <kris@krej.net>
2018-03-10 15:42:48 +0100

    SpawnPoint

---
+++ Assets/Code/SpawnPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+    public GameObject playerPrefab;
+    GameObject player;
+    static SpawnPoint instance;
+
+    void Awake() {
+        instance = this;
+        player = Instantiate(playerPrefab);
+        ResetPlayerPosition();
+        player.name = "Player";
+        gameObject.SetActive(false);
+    }
+
+    public static void ResetPlayerPosition() {
+        if (instance == null) {
+            Debug.LogError("No spawn point");
+            return;
+        }
+        instance.player.transform.position = instance.transform.position;
+        instance.player.transform.rotation = instance.transform.rotation;
+        instance.player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+}
