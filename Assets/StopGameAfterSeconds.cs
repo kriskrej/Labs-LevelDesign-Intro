e8ce32d5e1d64e296dd7acecb2a6976b35b635b9
Kris Krej <kris@krej.net>
2018-03-10 17:38:10 +0100

    Level complete!

---
+++ Assets/StopGameAfterSeconds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopGameAfterSeconds : MonoBehaviour {
+    float seconds = 1f;
+
+	void Start () {
+	    Invoke("Stop", seconds);	
+	}
+	
+	
+	void Stop () {
+        #if UNITY_EDITOR
+	    UnityEditor.EditorApplication.isPlaying = false;
+	     #endif
+        Debug.Log("Level complete, stopping playmode\n");
+    }
+}
---
+++ Assets/TriggerLevelComplete.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLevelComplete : MonoBehaviour {
+    public GameObject endGameUiAndLogic;
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Player")
+            endGameUiAndLogic.SetActive(true);
+    }
+}
