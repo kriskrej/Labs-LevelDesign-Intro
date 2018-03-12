07a01274abaf04e083b4f63fed16180501c49a3f
Kris Krej <kris@krej.net>
2018-03-12 18:02:29 +0100

    cleanup + student dir creator

---
+++ Assets/Code/Editor/StudentDirWindow.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+public class StudentDirWindow : EditorWindow {
+
+    [MenuItem("WSEI/Katalog studenta")]
+    public static void ShowWindow() {
+        EditorWindow.GetWindow(typeof(StudentDirWindow));
+    }
+
+    string name, surname, index;
+
+    void OnGUI() {
+        GUILayout.Label("Mój folder", EditorStyles.boldLabel);
+        name = ValidateString(EditorGUILayout.TextField("Imię", name));
+        surname = ValidateString(EditorGUILayout.TextField("Nazwisko", surname));
+        index = ValidateString(EditorGUILayout.TextField("Nr indeksu", index));
+        if (GUILayout.Button("Stwórz pusty katalog")) {
+            System.IO.Directory.CreateDirectory(GetStudendDirPath());
+            AssetDatabase.Refresh();
+        }
+    }
+
+    string GetStudendDirPath() {
+        return Application.dataPath + "/" + GetStudentDirName();
+    }
+
+    string ValidateString(string text) {
+        text = Regex.Replace(text, "[^\\w\\._]", "");
+        return text;
+    }
+
+    string GetStudentDirName() {
+        return index + "-" + name + "_" + surname;
+    }
+}
