5d64c7484896996ee68bc87994108cec344a23e5
Kris Krej <kris@krej.net>
2020-03-03 23:12:46 +0100

    error log removed

--- Assets/Standard Assets/Moe Baker/Moe Tools/Standalone/Utility/Data/PlayerPrefsX.cs
+++ Assets/Standard Assets/Moe Baker/Moe Tools/Standalone/Utility/Data/PlayerPrefsX.cs
@@ -4,38 +4,29 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters.Binary;
-
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.AI;
-
 #if UNITY_EDITOR
 using UnityEditor;
 using UnityEditorInternal;
 #endif
-
 using Object = UnityEngine.Object;
 using Random = UnityEngine.Random;
 
-namespace Moe.Tools
-{
-    public static class PlayerPrefsX
-    {
+namespace Moe.Tools {
+    public static class PlayerPrefsX {
         public static Dictionary<string, object> Dictionary { get; private set; }
-        public static string SavePath
-        {
-            get
-            {
-                return Path.Combine(Application.persistentDataPath, "Player Prefs X.dat");
-            }
+
+        public static string SavePath {
+            get { return Path.Combine(Application.persistentDataPath, "Player Prefs X.dat"); }
         }
 
         public static BinaryFormatter Formatter { get; private set; }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        static void Start()
-        {
+        static void Start() {
             Dictionary = new Dictionary<string, object>();
             Formatter = new BinaryFormatter();
 
@@ -45,117 +36,93 @@ namespace Moe.Tools
                 Save();
         }
 
-        public static void Save()
-        {
-            using (FileStream fs = new FileStream(SavePath, FileMode.OpenOrCreate))
-            {
+        public static void Save() {
+            using (FileStream fs = new FileStream(SavePath, FileMode.OpenOrCreate)) {
                 Formatter.Serialize(fs, Dictionary);
             }
         }
-        public static void Load()
-        {
-            using (FileStream fs = new FileStream(SavePath, FileMode.Open))
-            {
-                try
-                {
+
+        public static void Load() {
+            using (FileStream fs = new FileStream(SavePath, FileMode.Open)) {
+                try {
                     Dictionary = Formatter.Deserialize(fs) as Dictionary<string, object>;
-                }
-                catch (Exception)
-                {
-                    Debug.LogError("Error While Loading Player Prefs X, Resetting");
-
+                } catch (Exception) {
+                    //Debug.LogError("Error While Loading Player Prefs X, Resetting");
                     Dictionary = new Dictionary<string, object>();
-
                     fs.Close();
-
                     Save();
                 }
             }
         }
 
-        public static void Add(string ID, object obj)
-        {
-            if (CheckSerialization(obj))
-            {
+        public static void Add(string ID, object obj) {
+            if (CheckSerialization(obj)) {
                 if (Dictionary.ContainsKey(ID))
                     Dictionary[ID] = obj;
                 else
                     Dictionary.Add(ID, obj);
 
                 Save();
-            }
-            else
-            {
+            } else {
                 var type = obj.GetType();
 
-                Debug.LogError("Cant Serialize Type (" + type.FullName + "), Its Either Not Marked Serializable Or Has Memebers Not Marked Serializable");
+                Debug.LogError("Cant Serialize Type (" + type.FullName +
+                               "), Its Either Not Marked Serializable Or Has Memebers Not Marked Serializable");
             }
         }
-        public static bool CheckSerialization(object obj)
-        {
+
+        public static bool CheckSerialization(object obj) {
             if (!obj.GetType().IsSerializable) return false;
 
-            using (MemoryStream ms = new MemoryStream())
-            {
-                try
-                {
+            using (MemoryStream ms = new MemoryStream()) {
+                try {
                     Formatter.Serialize(ms, obj);
 
                     return true;
-                }
-                catch (Exception)
-                {
+                } catch (Exception) {
                     return false;
                 }
             }
         }
 
-        public static bool Remove(string ID)
-        {
-            if (IsDefined(ID))
-            {
+        public static bool Remove(string ID) {
+            if (IsDefined(ID)) {
                 Dictionary.Remove(ID);
 
                 Save();
 
                 return true;
-            }
-            else
-            {
+            } else {
                 Debug.LogError("Pref With ID : " + ID + " Not Defined");
 
                 return true;
             }
         }
 
-        public static bool IsDefined(string ID)
-        {
+        public static bool IsDefined(string ID) {
             return Dictionary.ContainsKey(ID);
         }
 
-        public static object Get(string ID)
-        {
+        public static object Get(string ID) {
             return Get<object>(ID);
         }
-        public static T Get<T>(string ID)
-        {
+
+        public static T Get<T>(string ID) {
             return Get(ID, default(T));
         }
-        public static T Get<T>(string ID, T defaultValue)
-        {
+
+        public static T Get<T>(string ID, T defaultValue) {
             if (Dictionary.ContainsKey(ID))
-                return (T)Dictionary[ID];
+                return (T) Dictionary[ID];
             else
                 return defaultValue;
         }
 
-        public static void Set(string ID, object obj)
-        {
+        public static void Set(string ID, object obj) {
             Add(ID, obj);
         }
 
-        public static void Clear()
-        {
+        public static void Clear() {
             Dictionary.Clear();
 
             Save();
